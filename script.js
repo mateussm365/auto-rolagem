84254979cbed63f2e4049bd264fdcc2e9ada4ed7
mateussm365 <mateussm365@gmail.com>
2025-04-15 16:43:09 -0300

    fimaula17

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log('Hello!');
+function rolagem (id_secao){
+    const elemento = document.getElementById(id_secao);
+
+if(elemento){
+
+}
+
+}
+
+elemento.scrollIntoView({ behavior: 'smooth' }); 
