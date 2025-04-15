04d0a3eb3b55d7766bf1c41289a9948c6eef09c1
mateussm365 <mateussm365@gmail.com>
2025-04-15 16:47:12 -0300

    fim

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ function rolagem (id_secao){
     const elemento = document.getElementById(id_secao);
 
 if(elemento){
-
+    elemento.scrollIntoView({ behavior: 'smooth' }); 
 }
 
 }
 
-elemento.scrollIntoView({ behavior: 'smooth' }); 
+window.rolagem = rolagem
