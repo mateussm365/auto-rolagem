521233ed9a1d9b5a0752a5e90f90a27a0cea10b2
mateussm365 <mateussm365@gmail.com>
2025-04-24 12:31:40 -0300

    fim da aula 18

--- script.js
+++ script.js
@@ -8,3 +8,11 @@ if(elemento){
 }
 
 window.rolagem = rolagem
+
+function func_overlay(elemento, i)
+
+const text_over = document.getElementsByClassName("overlay-text");
+text_over[i].style.width = elemento.offsetWidth + "px";
+text_over[i].style.height = elemento.offsetHeight + "px";
+
+window.func_overlay = func_overlay;
