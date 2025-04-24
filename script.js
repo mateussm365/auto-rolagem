3f763da8a7b25a87976e632c5db044f6e75989ee
mateussm365 <mateussm365@gmail.com>
2025-04-24 12:41:33 -0300

    ss s

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-function rolagem (id_secao){
+function rolagem(id_secao) {
     const elemento = document.getElementById(id_secao);
 
-if(elemento){
-    elemento.scrollIntoView({ behavior: 'smooth' }); 
-}
+    if (elemento) {
+        elemento.scrollIntoView({ behavior: 'smooth' });
+    }
 
 }
 
 window.rolagem = rolagem
 
-function func_overlay(elemento, i)
+function func_overlay(elemento, i) {
+    const text_over = document.getElementsByClassName("overlay-text");
+    text_over[i].style.width = elemento.offsetWidth + "px";
+    text_over[i].style.height = elemento.offsetHeight + "px";
 
-const text_over = document.getElementsByClassName("overlay-text");
-text_over[i].style.width = elemento.offsetWidth + "px";
-text_over[i].style.height = elemento.offsetHeight + "px";
+}
 
-window.func_overlay = func_overlay;
+
