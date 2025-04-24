a19efe7a36273d0042b58fd4e92bbce5abbd4774
mateussm365 <mateussm365@gmail.com>
2025-04-24 12:43:15 -0300

    Final aula 18

--- script.js
+++ script.js
@@ -13,7 +13,8 @@ function func_overlay(elemento, i) {
     const text_over = document.getElementsByClassName("overlay-text");
     text_over[i].style.width = elemento.offsetWidth + "px";
     text_over[i].style.height = elemento.offsetHeight + "px";
-
 }
+
+window.func_overlay = func_overlay;
 
 
